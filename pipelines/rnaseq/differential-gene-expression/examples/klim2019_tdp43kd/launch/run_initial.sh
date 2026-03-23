3fe0637fbb0e39f8dde530e4fcc23850c428e688
white_lab_neuro <75120055+whitelabneuro@users.noreply.github.com>
2026-03-23 14:23:37 +0000

    Create run_initial.sh

---
+++ pipelines/rnaseq/differential-gene-expression/examples/klim2019_tdp43kd/launch/run_initial.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+PIPELINE_VERSION="1.5.0"
+PROFILE="singularity"
+
+nextflow run nf-core/differentialabundance \
+  -r "${PIPELINE_VERSION}" \
+  -profile "${PROFILE}" \
+  -params-file launch/params.initial.yaml \
+  -c launch/create.config
